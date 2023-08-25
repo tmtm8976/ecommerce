7726dd47c59aef875582e9cf179b60ce0ad1e652
tmtm8976 <fatemayasser8976@gmail.com>
2023-08-25 18:22:53 +0300

    styling products details

--- src/components/products/ProductDetails.jsx
+++ src/components/products/ProductDetails.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { getProductById } from '../../services/product.service'
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 export const ProductDetails = () => {
     const {id} = useParams()
@@ -14,15 +17,36 @@ export const ProductDetails = () => {
         getProduct(id)
     }, [])
 
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
+
   return (<>
-      <div>{JSON.stringify(product)}</div>
-      {product  && <div className='flex justify-center'>
-        <div className='relative w-1/3 h-[min(50vh,400px)] overflow-hidden rounded-md'>
-          {product.images.map(img=><img src={img} className='absolute top-0 left-0 h-full object-cover'/>)}
-        </div>
+      {/* <div>{JSON.stringify(product)}</div> */}
+      {product  && <div className='flex justify-center p-32 gap-16 relative '>
+        {/* <div className='h-[300px] w-full absolute p-5 bg-[#2e2e2e] top-48 -z-10'></div> */}
+      <div className='relative w-1/3 h-[min(50vh,400px)] rounded-md '>
+      <Slider {...settings} >
+    {product.images.map((img, index) => (
+      <div className='h-full overflow-hidden'><img key={index} src={img} className='h-full object-cover' /></div>
+    ))}
+  </Slider>
+</div>
         <div>
-          <h1>{product.title}</h1>
+         <div className=''><p>Rating: <span className='text-yellow-700'>{product.rating}</span></p><h1 className='m-0 font-bold text-3xl'>{product.title}</h1></div> 
+        <hr className='w-full pt-[0.5px] my-5 border-0 bg-[#fe9900]' />
           <p>{product.description}</p>
+          <span className='font-bold'>{product.price+" $"}</span>
+          <p className='text-red-600'>{"BUY NOW TO GET "+product.discountPercentage+"% OFF"}</p>
+          
+          <p>Stock: <span className='text-green-500'>{product.stock}</span></p>
+          <p>Brand: <span>{product.brand}</span></p>
+          <p>{"Category: "+product.category}</p>
+          <button className='px-5 py-3 bg-[#fe9900] rounded-sm mt-8 shadow-md font-bold text-white hover:scale-105 hover:ease-in-out hover:transition-transform'>Buy now</button>
         </div>
       </div>}
     </>
