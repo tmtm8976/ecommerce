ad389d02d79c92742c1846114681bd29699fad58
tmtm8976 <fatemayasser8976@gmail.com>
2023-08-26 16:23:38 +0300

    fill about us

--- src/pages/Auth/LoginForm.jsx
+++ src/pages/Auth/LoginForm.jsx
@@ -3,7 +3,7 @@ import Login from '../../components/Auth/Login'
 
 const LoginForm = () => {
   return (
-    <div className='d-flex align-items-center'>
+    <div className='d-flex pb-24 align-items-center !h-[max(100%,90vh)]'>
       <div className='w-50'>
     <Login/>
       </div>
--- src/pages/statics/About.jsx
+++ src/pages/statics/About.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
 
+const aboutUsData = [
+  {
+    title: 'Our Story',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget nisl vel risus suscipit tincidunt. Nullam auctor commodo nunc, vitae venenatis augue pretium et.'
+  },
+  {
+    title: 'Our Mission',
+    content: 'Praesent vel nisl euismod, bibendum mi sed, cursus metus. Nulla facilisi. Integer auctor velit nec sapien pharetra, sit amet consectetur elit euismod.'
+  },
+  {
+    title: 'Our Team',
+    content: 'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla facilisi. Cras sagittis ligula ut ante congue, vel consequat nisi ullamcorper.'
+  }
+]
+
 export const About = () => {
   return (
-    <div>About</div>
+    <div className="bg-white h-[75vh]">
+      <div className="container mx-auto py-8 px-4">
+        <h1 className="text-3xl font-bold mb-4">About Us</h1>
+        <hr className='w-full pt-[0.5px] mb-5 border-0 bg-[#fe9900]' />
+        {aboutUsData.map((item, index) => (
+          <div key={index} className="mb-8 py-7">
+            <h2 className="text-2xl font-bold mb-2 ">{item.title}</h2>
+            <p className="text-gray-700">{item.content}</p>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
